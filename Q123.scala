3c603b81dadc575d57bf4c75c6ee2e3f7967c44d
lasinduwathsan <109015894+lasinduwathsan@users.noreply.github.com>
2022-07-10 22:09:48 +0530

    i added Q123 file

---
+++ Q123.scala
@@ -0,0 +1,21 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+    val k:Int =2
+    var i:Int =2
+    var j:Int =2
+    var m:Int =5
+    var n:Int =5
+    var f:Float =12.0f
+    var g:Float=4.0f
+    var c:Char='X'
+
+    println("Value is:"+(k+12*m))
+    println("Value is:"+(m/j))
+    println("Value is:"+(n%j))
+    println("Value is:"+(m/j*j))
+    println("Value is:"+(f+10*5+g))
+
+    i=i+1
+    println("Value is:"+i)
+  }
+}
